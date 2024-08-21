408614e28235c70d0e16bbca7f1ef516b5b610ce
ApheeeeKhambule <ApheleleKhambule16@gmail.com>
2024-08-21 11:38:09 +0200

    new

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    INSTALLED_APPS = [
-    # Other installed apps...
-    'bookshelf',
-]
 ]
 
 MIDDLEWARE = [
