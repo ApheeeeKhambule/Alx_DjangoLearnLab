60d699dbe51b92728e91f84c70151b80a9b9d79a
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-08-28 17:32:42 +0200

    new

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,9 @@
 
 # api/views.py
-from rest_framework.generics import ListAPIView  # Correct import
+from rest_framework.generics.ListAPIView  # Correct import from DRF generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):  # Correct usage of ListAPIView
+class BookList(ListAPIView):  # Correctly extending ListAPIView
     queryset = Book.objects.all()
     serializer_class = BookSerializer
