3a53a64e97e232c371644f3063421a0211947e54
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-08-29 19:48:15 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,31 @@
+# LibraryProject/bookshelf/admin.py
+
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # Import your custom user model
+from django.utils.translation import gettext_lazy as _
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
+# Define a custom admin class for your custom user model
+class CustomUserAdmin(UserAdmin):
+    # Customizing the fields displayed in the admin interface
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
 
-admin.site.register(Book, BookAdmin)
+# Register the custom user model and the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
+
