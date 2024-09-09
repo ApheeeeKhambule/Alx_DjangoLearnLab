206b2583af0958a3a60415c82f7815fb44f472ac
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-09 15:48:04 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,3 @@
-# blog/urls.py
-from django.urls import path
-from accounts import views as account_views
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
@@ -10,11 +7,4 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-]
-
-urlpatterns = [
-    path('register/', account_views.register, name='register'),
-    path('login/', account_views.login_view, name='login'),
-    path('logout/', account_views.logout_view, name='logout'),
-    path('profile/', account_views.profile, name='profile'),
 ]
