b622c21b9faa2369a2fa9d5666b7ae3e5d692bc1
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-09 14:14:48 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,18 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 
 class CustomLoginView(LoginView):
     template_name = 'blog/login.html'
