4576237619517409c657f72c3d04bc69b6a6f024
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-11 17:32:00 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,42 +1,29 @@
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
-from .forms import PostForm
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'post_list.html'
-    context_object_name = 'posts'
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-    context_object_name = 'post'
-
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'post_form.html'
-    success_url = reverse_lazy('post-list')
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'post_form.html'
-    success_url = reverse_lazy('post-list')
-
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
-
-class PostDeleteView(DeleteView):
-    model = Post
-    template_name = 'post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')
-
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'profile.html', {'form': form})
