ea702c5dc4f74b1dc35bc74e8f683e69216bc0fa
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-12 12:12:59 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
 # Create a view that renders the home.html template
 def home(request):
     return render(request, 'blog/home.html')
-
