734abf872ce1682701f4596f0b0909ab9b6f7447
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-12 15:15:29 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,6 +5,14 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import Post
 
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
