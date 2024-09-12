4ff4eda6a1e82b61b3e28384089c0e755aad86f3
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-12 12:25:07 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+   path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+   path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+   path('register/', views.register, name='register'),
+   path('profile/', views.profile, name='profile'),
+]
 
 urlpatterns = [
     path('', views.home, name='home'),  # Link the view to the home page
