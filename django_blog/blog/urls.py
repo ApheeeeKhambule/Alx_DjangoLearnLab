06aab7aa1cf7303925b5474fb97138b9eba11ce9
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-13 21:52:36 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,5 @@
 
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import add_comment, edit_comment, delete_comment
-from django.urls import path
 from .views import (
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 )
@@ -14,17 +11,4 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
-
-urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
-    path('comments/<int:pk>/edit/', edit_comment, name='edit-comment'),
-    path('comments/<int:pk>/delete/', delete_comment, name='delete-comment'),
-]
-
-
 
