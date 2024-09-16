eb550e6218fcea97a8d5d6840dd1d346b3990d67
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-16 21:57:28 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,38 +1,27 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
+from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 
-# Get the custom user model
-CustomUser = get_user_model()
-
-class RegisterSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'password', 'password2', 'email')
+        fields = ['username', 'password', 'password2', 'email', 'bio', 'profile_picture']
 
-    # Validate that the passwords match
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Passwords do not match."})
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
         return attrs
 
-    # Create a user and also generate a token for the user
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
+        user = CustomUser(
             email=validated_data['email'],
-            password=validated_data['password']
+            username=validated_data['username'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
-
-        # Create an authentication token for the user
-        Token.objects.create(user=user)
+        user.set_password(validated_data['password'])
+        user.save()
         return user
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
