395c98bf1e47d3ac46454c42576aec09f8d9c8f4
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 20:46:24 +0200

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,3 +14,4 @@ class LibraryDetailView(DetailView):
  ["relationship_app/list_books.html"]
 ["relationship_app/library_detail.html", "from .models import Library"]
  ["from django.views.generic.detail import DetailView"]
+ ["from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm"]
