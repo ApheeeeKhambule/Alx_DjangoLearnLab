dc84c86a6ec0ef86631e86509dac86704f4d755a
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 18:08:36 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,7 @@ from . import views
  ["login/", "register/", "profile/"]
 ["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"]
 
+ ["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
 from django.urls import path
 from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
