f4308e19425b198c99c638a122a44d9af3cdc542
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 12:52:43 +0200

    Add files via upload

---
+++ social_media_api/README.md
@@ -0,0 +1,10 @@
+Social Media API
+Overview
+The Social Media API is a backend system that allows users to register, authenticate, create posts, comment on posts, and manage content. Built using Django and Django REST Framework (DRF), the API supports user authentication, CRUD operations for posts and comments, and token-based authentication for secure access.
+
+Features
+User registration and login
+Token-based authentication
+Create, retrieve, update, and delete (CRUD) functionality for posts and comments
+Pagination and filtering for posts
+Permissions to ensure users can only edit/delete their own posts and comments
---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'bio', 'profile_picture', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            bio=validated_data['bio'],
+            profile_picture=validated_data.get('profile_picture'),
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
