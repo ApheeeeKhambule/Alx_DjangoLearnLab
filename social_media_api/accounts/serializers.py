872a31898b06bc342bc7b17f15ec2ab6fac57f31
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 12:11:44 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,38 +1,26 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
-from rest_framework.authtoken.models import Token
 
-# Get the custom user model
-User = get_user_model()
+# Getting the custom user model
+CustomUser = get_user_model()
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    # Using CharField for passwords to enforce write-only access
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+class RegisterSerializer(serializers.ModelSerializer):
+    # Explicitly declaring the password field as a write-only field
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
-        fields = ['username', 'password', 'password2', 'email', 'bio', 'profile_picture']
+        model = CustomUser
+        # Including fields for registration
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
-    # Validating that both password fields match
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
-    # Custom method for creating a new user
+    # Overriding the create method to handle password hashing
     def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
+        # Using the create_user method provided by Django for user creation
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],  # Password will be hashed automatically
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        user.set_password(validated_data['password'])  # Set hashed password
-        user.save()
-
-        # Create a token for the new user
-        Token.objects.create(user=user)
-
         return user
