031ef19b73064410377e713c197d182690986069
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 13:23:23 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -6,28 +6,22 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Post
-from accounts.models import CustomUser
 from .serializers import PostSerializer
 
-# A view for displaying a user's feed based on who they follow
 class UserFeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # Get the users that the current user is following
         following_users = self.request.user.following.all()
-        # Filter posts from the users that the current user follows, ordered by creation date
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
-# Standard CRUD views for posts (you can modify based on your needs)
 class PostCreateView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Set the author of the post to the current user
         serializer.save(author=self.request.user)
 
 class PostUpdateView(generics.UpdateAPIView):
@@ -36,7 +30,6 @@ class PostUpdateView(generics.UpdateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_update(self, serializer):
-        # Ensure that only the author can update their post
         if serializer.instance.author != self.request.user:
             raise PermissionDenied("You do not have permission to edit this post.")
         serializer.save()
@@ -47,7 +40,6 @@ class PostDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_destroy(self, instance):
-        # Ensure that only the author can delete their post
         if instance.author != self.request.user:
             raise PermissionDenied("You do not have permission to delete this post.")
         instance.delete()
