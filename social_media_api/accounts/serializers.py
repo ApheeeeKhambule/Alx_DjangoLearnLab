bdb86c009121f1b8d132c1b565f218b9d5371e12
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 13:29:03 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,4 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth import authenticate
-
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
@@ -28,29 +24,4 @@ class UserSerializer(serializers.ModelSerializer):
         Token.objects.create(user=user)
         return user
 
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'bio', 'profile_picture', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            bio=validated_data['bio'],
-            profile_picture=validated_data.get('profile_picture'),
-            password=validated_data['password']
-        )
-        return user
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Incorrect Credentials")
+  
