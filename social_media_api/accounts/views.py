c9158a3097aa93dccb166ba5d1932958cda13b0b
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 13:04:25 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,28 +5,36 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from .serializers import UserSerializer, LoginSerializer
 from .models import CustomUser
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-class FollowUserView(APIView):
+# A view to list all users (example usage of generics.GenericAPIView and CustomUser.objects.all())
+class UserListView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, user_id):
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+# A view that requires the user to be authenticated (example usage of permissions.IsAuthenticated)
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id, *args, **kwargs):
         user_to_follow = get_object_or_404(CustomUser, id=user_id)
         request.user.following.add(user_to_follow)
-        return Response({'detail': 'You are now following this user.'}, status=status.HTTP_200_OK)
+        return Response({'detail': 'You are now following this user.'})
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, user_id):
+    def post(self, request, user_id, *args, **kwargs):
         user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
         request.user.following.remove(user_to_unfollow)
-        return Response({'detail': 'You have unfollowed this user.'}, status=status.HTTP_200_OK)
+        return Response({'detail': 'You have unfollowed this user.'})
 
 
 class RegisterUser(APIView):
