f1da23a5dbecda481cea86e81f9a9b289391e917
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-19 15:55:41 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),    
 ]
 ["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
+ ["add_book/", "edit_book/", "delete_book"]
